5ecb6dbc4cad0d31941e832b3f30c14131bea40a
GianLost <gianluca19993m@gmail.com>
2025-11-28 16:33:45 -0300

    feat: implementação da barra de medição de força de senhas + melhorias e correções de estilo e design nas páginas e diálogos nos modelos de setores e usuários.

--- SIP.UI/Domain/DTOs/Users/Configurations/AdminResetPasswordDTO.cs
+++ SIP.UI/Domain/DTOs/Users/Configurations/AdminResetPasswordDTO.cs
@@ -12,6 +12,6 @@ public class AdminResetPasswordDTO
 
     [Required(ErrorMessage = "A senha é obrigatória.")]
     [StringLength(255, MinimumLength = 8, ErrorMessage = "A senha deve ter entre 8 e 255 caracteres.")]
-    [RegularExpression(ConstExpressions.StrongPasswordRegex, ErrorMessage = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial.")]
+    [RegularExpression(ConstExpressions.StrongPasswordRegex, ErrorMessage = "A senha deve conter letras, números e símbolos.")]
     public string Password { get; set; } = string.Empty;
 }
